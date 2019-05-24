eff34e34be196ca184abfc44c37404ace9d6ff72
github username <github email address>
2019-05-24 15:23:11 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,18 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-  end
+   #  Artist.all.first.genre
+   gen_id = Song.all.select{|inst| inst.artist_id == self.id}[0].genre_id
+   Genre.all.select{|inst| inst.id == gen_id}[0]
+end
 
-  def song_count
-    #return the number of songs associated with the artist
-  end
+def song_count
+   #return the number of songs associated with the artist
+   Song.all.select{|inst| inst.artist_id == self.id}.count
+end
 
-  def genre_count
-    #return the number of genres associated with the artist
+def genre_count
+   #return the number of genres associated with the artist
+   Song.all.select{|inst| inst.artist_id == self.id}.map{|inst| inst.genre_id}.uniq.count
   end
 end
